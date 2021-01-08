87d05a90e6d854e48dfe5bb3ac0ae9b2a1b42ecf
wildnei <wildneisuane@gmail.com>
2021-01-07 23:00:14 -0500

    adding for and while to validate the chosen numbers

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
 // DOM elements
 
 const resultElement = document.getElementById("result");
@@ -22,17 +13,20 @@ const generateElement = document.getElementById("generate");
 // Calling the function after click
 
 generateElement.addEventListener('click', function () {
-  confirmingQuestions ()
-  });
+  confirmingQuestions()
+});
 
+// Prompts for questions
 
-function confirmingQuestions () {
+function confirmingQuestions() {
   var confirmLength = prompt("Please select a number between 8 and 128")
   if (confirmLength < 8 || confirmLength > 128) {
-     confirmingQuestions ();
+    confirmingQuestions();
   };
 
   var confirmUpperCase = confirm("Allow upper case?");
+
+  var confirmLowerCase = confirm("Allow lower case?");
 
   var confirmNumberCase = confirm("Add number?");
 
@@ -40,93 +34,94 @@ function confirmingQuestions () {
 
   var genPassword = "";
 
+  // This while makes the values loop until it has validate the answer from true or false
+
   for (let index = 0; index < confirmLength; index++) {
-    // const element = array[index];
-    switch (randomPasswordSelection()) {
-      case 1: 
-        genPassword += getRandomUpper();
-        break;
-      case 2:
-        genPassword += getRandomNumber();
-        break;
-      case 3:
-        genPassword += getRandomSpecialCharacter();
-        break;
-      case 4:
-        genPassword += getRandomUpper();
-        break;
-      default:
+
+    var randNumber = randomPasswordSelection();
+    var x = true
+    while (x) {
+      x = false
+
+      console.log(randNumber, confirmLowerCase);
+
+      if (randNumber === 1 && confirmLowerCase === false) {
+
+        randNumber = randomPasswordSelection();
+        x = true;
+      }
+
+      if (randNumber === 2 && confirmUpperCase === false) {
+        randNumber = randomPasswordSelection();
+        x = true;
+      }
+
+      if (randNumber === 3 && confirmNumberCase === false) {
+        randNumber = randomPasswordSelection();
+        x = true;
+      }
+
+      if (randNumber === 4 && confirmSpecialCase === false) {
+        randNumber = randomPasswordSelection();
+        x = true;
+      }
+
+    }
+
+    switch (randNumber) {
+      case 1:
         genPassword += getRandomLower();
         break;
+      case 2:
+        genPassword += getRandomUpper();
+        break;
+      case 3:
+        genPassword += getRandomNumber();
+        break;
+      case 4:
+        genPassword += getRandomSpecialCharacter();
+        break;
     }
+
     genPassword += ""
-  }
+  };
+
+  var displayPassword = document.querySelector("#password");
+  displayPassword.textContent = genPassword
+
   console.log(genPassword);
-  console.log(randomPasswordSelection())
+  console.log(randomPasswordSelection());
+
 };
 
-
-
-function randomPasswordSelection (allowUpper, allowNumber, allowSpecial) {
- return Math.floor(Math.random() * 5) +1 
+function randomPasswordSelection(allowUpper, allowNumber, allowSpecial) {
+  return Math.floor(Math.random() * 4) + 1
 };
-
-// Loop how many times it goes 
-
-/* Loop for character length
-If type = true(Special, char, number, Lower, upper)
-Grab a random char for that type
-Append / cocat to global string variable(+=)
-
-*/
-
-
- 
-// const randomFunction = {
-//   lower: getRandomLower,
-//   upper: getRandomUpper,
-//   number: getRandomNumber,
-//   special: getRandomSpecialCharacter
-// };
-
-
-
 
 // Generate lowercase letter
-
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
+console.log(getRandomLower());
 
 // Generate uppercase letter
-
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
+console.log(getRandomUpper());
 
 // Generate number
-
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 };
+console.log(getRandomNumber());
 
 // Generate Special Character
-
 function getRandomSpecialCharacter() {
   const SpecialCharacter = "!@#$%^&*(){}[]=<>/.,";
   return SpecialCharacter[Math.floor(Math.random() * SpecialCharacter.length)];
 };
-
-
-// console logs
-
-console.log(getRandomUpper());
-console.log(getRandomNumber());
 console.log(getRandomSpecialCharacter());
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 
